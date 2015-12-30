01a739b72d3ce9bc5e62c55d6d6d4e51a78643ea
Aaron Frantisak <afrantisak@gmail.com>
2015-12-30 17:58:08 -0600

    working

--- main.py
+++ main.py
@@ -20,21 +20,31 @@ class FakeTerminal(QtGui.QTextEdit):
     
     def __init__(self,  parent):
         super(FakeTerminal,  self).__init__(parent)
+        self.new_text = ''
+        self.prompt = "$ "
+        self.insert_prompt()
 
     def keyPressEvent(self,  event):
         QtGui.QTextEdit.keyPressEvent(self,  event)
         if event.key() == Qt.Key_Return:
-            self.enterPressed.emit(self.toPlainText())
+            diff = self.get_text_difference()
+            self.enterPressed.emit(diff.toAscii().data().rstrip())
+            self.insert_prompt()
+
+    def get_text_difference(self):
+        self.old_text = self.new_text
+        self.new_text = self.toPlainText()
+        return self.new_text[len(self.old_text) + len(self.prompt):]
+
+    def insert_prompt(self):
+        self.insertPlainText(self.prompt)
 
 class MainWindow(QtGui.QDialog):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi()
 
-        self.challenge_number = 0
-        self.challenge = challenges[self.challenge_number]
-        self.challengeLabel.setText("Challenge: " + self.challenge['name'])
-        self.challengeText.setText(self.challenge['desc'])
+        self.display_challenge(0)
         self.terminalText.setFocus()
         self.show()
 
@@ -52,8 +62,31 @@ class MainWindow(QtGui.QDialog):
         self.terminalText.enterPressed.connect(self.handle_enter)
 
     def handle_enter(self, text):
-        print "ENTER", text
-        
+        print len(text), len(self.challenge['answer'])
+        if text == self.challenge['answer']:
+            next_challenge = self.challenge_number + 1
+            if next_challenge >= len(challenges):
+                self.message("You Win!", "good")
+                self.accept()
+                return
+            self.message("Nice", "good")
+            self.display_challenge(self.challenge_number + 1)
+        else:
+            self.message("Try again", "bad")
+
+    def message(self, text, message_type):
+        title = "Message"
+        if message_type == "good":
+            QtGui.QMessageBox.information(self, title, text)
+        else:
+            QtGui.QMessageBox.warning(self, title, text)
+
+    def display_challenge(self, challenge_number):
+        self.challenge_number = challenge_number
+        self.challenge = challenges[self.challenge_number]
+        self.challengeLabel.setText("Challenge: " + self.challenge['name'])
+        self.challengeText.setText(self.challenge['desc'])
+
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QtGui.QApplication(sys.argv)
